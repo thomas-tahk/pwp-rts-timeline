12092468e11653fba928c0a5aa83d4eabc0103c8
Thomas Tahk <ttahk@cnm.edu>
2025-02-20 07:42:10 -0700

    more layout progress, mostly in aside

--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
 import './index.css'
 import 'flowbite'
 
+let timeline = document.getElementById('timeline')
+let aside = document.getElementById('aside')
+function toggleDisplay(element) {
+    element.classList.toggle('hidden')
+}
+// add code to target which ones can trigger toggleDisplay or maybe that should be on
 
