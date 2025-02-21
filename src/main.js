2ddaead3b3ddbb986435c4eb0846e74565733445
Thomas Tahk <ttahk@cnm.edu>
2025-02-21 07:07:22 -0700

    mid-attempt for layout changes and toggling hidden

--- src/main.js
+++ src/main.js
@@ -1,10 +1,16 @@
 import './index.css'
 import 'flowbite'
 
-let timeline = document.getElementById('timeline')
-let aside = document.getElementById('aside')
-function toggleDisplay(element) {
-    element.classList.toggle('hidden')
+let testButtonTimeline = document.getElementById('testHiddenToggle')
+let testShowcase = document.getElementById('testToggle')
+
+function toggleDisplay() {
+    let timeline = document.getElementById('timeline')
+    let showcase = document.getElementById('showcase')
+    timeline.classList.toggle('hidden')
+    showcase.classList.toggle('hidden')
 }
-// add code to target which ones can trigger toggleDisplay or maybe that should be on
+
+testButtonTimeline.addEventListener('click', toggleDisplay)
+testShowcase.addEventListener('click', toggleDisplay)
 
