61a6c886159f692be0ab3da7157eefa8fbeb2b2a
Thomas Tahk <ttahk@cnm.edu>
2025-02-22 13:13:18 -0700

    the js module fix and few minor changes

--- src/main.js
+++ src/main.js
@@ -7,8 +7,22 @@ let testShowcase = document.getElementById('testToggle')
 function toggleDisplay() {
     let timeline = document.getElementById('timeline')
     let showcase = document.getElementById('showcase')
-    timeline.classList.toggle('hidden')
-    showcase.classList.toggle('hidden')
+    if (timeline.classList.contains('hidden')) {
+        timeline.classList.remove('hidden')
+        console.log('timeline was hidden')
+    } else {
+        timeline.classList.add('hidden')
+        console.log('timeline was not hidden')
+    }
+    if (showcase.classList.contains('hidden')) {
+        showcase.classList.remove('hidden')
+        console.log('showcase was hidden')
+    } else {
+        showcase.classList.add('hidden')
+        console.log('showcase was not hidden')
+    }
+    console.log("if I don't see anything before me, something wrong happened")
+
 }
 
 testButtonTimeline.addEventListener('click', toggleDisplay)
