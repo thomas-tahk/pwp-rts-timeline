1cce05017214ff10c630dc9b4b9155dee83d32e2
Thomas Tahk <ttahk@cnm.edu>
2025-03-08 20:37:47 -0700

    adding some content but also video is killing me

--- src/main.js
+++ src/main.js
@@ -1,6 +1,43 @@
 import './index.css'
 import 'flowbite'
 
+let tag = document.createElement('script');
+
+tag.src = "https://www.youtube.com/iframe_api";
+let firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+let rtsOriginsVideo;
+function onYouTubeIframeAPIReady() {
+    rtsOriginsVideo = new YT.Player('rts-origins-video', {
+        height: '390',
+        width: '640',
+        videoId: 'IEXKZ-dzZJI',
+        playerVars: {
+            'playsinline': 1
+        },
+        events: {
+            'onReady': onPlayerReady,
+            'onStateChange': onPlayerStateChange
+        }
+    });
+}
+
+function onPlayerReady(event) {
+    event.target.playVideo();
+}
+
+let done = false;
+function onPlayerStateChange(event) {
+    if (event.data == YT.PlayerState.PLAYING && !done) {
+        setTimeout(stopVideo, 6000);
+        done = true;
+    }
+}
+function stopVideo() {
+    player.stopVideo();
+}
+
 // let testButtonTimeline = document.getElementById('testHiddenToggle')
 // let testShowcase = document.getElementById('testToggle')
 //
